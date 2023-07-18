4c4d5955b62f098eadae89028a5a1e3c9e975935
MILIND <milindanand786@gmail.com>
2023-07-19 01:13:46 +0530

    Initial commit

---
+++ src/components/navbar/burger/burger.jsx
@@ -0,0 +1,11 @@
+const Burger = ({ onClick }) => {
+  return (
+    <button onClick={onClick} className="bg">
+      <span />
+      <span />
+      <span />
+    </button>
+  );
+};
+
+export default Burger;
